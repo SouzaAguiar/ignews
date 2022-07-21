cae639b60fa0d7f0a75ae7aee7c8c33e5439ff81
jonathan <johnathan.souza@gmail.com>
2022-07-21 20:35:46 -0300

    log 5

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -33,7 +33,7 @@ export default async function webHooks(
 
     try {
       event = stripe.webhooks.constructEvent(
-        buf,
+        req.body,
         secret,
         process.env.STRIPE_WEBHOOK_SECRET
       );
