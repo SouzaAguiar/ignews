e8f26233c5a2dde696191c496a1a01fb285b0c59
jonathan <johnathan.souza@gmail.com>
2022-07-21 20:04:11 -0300

    add stripe log

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -38,6 +38,7 @@ export default async function webHooks(
         process.env.STRIPE_WEBHOOK_SECRET
       );
     } catch (error) {
+      console.log(error);
       return res.status(400).send(`Webhook Error: ${error.message}`);
     }
     const { type } = event;
