715091d4de394bc3ca081c2655ce94b4c885f58b
jonathan <johnathan.souza@gmail.com>
2022-07-21 21:29:14 -0300

    log web hook

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -39,7 +39,7 @@ export default async function webHooks(
         process.env.STRIPE_WEBHOOK_SECRET
       );
     } catch (error) {
-      console.log(error);
+      console.log(process.env.STRIPE_WEBHOOK_SECRET);
       return res.status(400).send(`Webhook Error: ${error.message}`);
     }
     const { type } = event;
